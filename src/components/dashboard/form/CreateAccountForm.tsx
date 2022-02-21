a68cc2182f79c6e5c8821d49a6380ef09944bb6c
Julian <42209193+juliarn@users.noreply.github.com>
2022-02-21 23:49:16 +0100

    Use FormFields in create account form too

--- src/components/dashboard/form/CreateAccountForm.tsx
+++ src/components/dashboard/form/CreateAccountForm.tsx
@@ -1,5 +1,6 @@
-import React, {ChangeEvent, FormEvent, useState} from 'react';
+import React, {FormEvent, useState} from 'react';
 import {Link} from 'react-router-dom';
+import FormField from './FormField';
 
 const CreateAccountForm = () => {
   const [username, setUsername] = useState<string>('');
@@ -24,60 +25,42 @@ const CreateAccountForm = () => {
     event.preventDefault();
   };
 
-  const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setUsername(event.target.value);
-    setUsernameError('');
-  };
-
-  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
-    setPasswordError('');
-  };
-
   return (
     <div className="w-full max-w-xs">
       <form
         onSubmit={handleRegister}
         className="bg-white shadow-md rounded px-6 pt-6 pb-8 mb-4"
       >
-        <div className="mb-6">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="password"
-          >
-            Password
-          </label>
-          <input
-            value={password}
-            onChange={handlePasswordChange}
-            className={`shadow appearance-none border ${
-              passwordError ? 'border-red-500' : ''
-            } rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
-            id="password"
-            type="password"
-            placeholder="Password"
-          />
-          <p className="text-red-500 text-xs italic">{passwordError}</p>
-        </div>
-        <div className="mb-6">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="repeatPassword"
-          >
-            Repeat Password
-          </label>
-          <input
-            value={repeatPassword}
-            onChange={handlePasswordChange}
-            className={`shadow appearance-none border ${
-              passwordError ? 'border-red-500' : ''
-            } rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
-            id="repeatPassword"
-            type="password"
-            placeholder="Repeat Password"
-          />
-          <p className="text-red-500 text-xs italic">{passwordError}</p>
-        </div>
+        <FormField
+          name={'Username'}
+          value={username}
+          valueSetter={setUsername}
+          valueError={usernameError}
+          valueErrorSetter={setUsernameError}
+        />
+        <FormField
+          name={'First Name'}
+          value={firstName}
+          valueSetter={setFirstName}
+          valueError={firstNameError}
+          valueErrorSetter={setFirstNameError}
+        />
+        <FormField
+          name={'Password'}
+          type={'password'}
+          value={password}
+          valueError={passwordError}
+          valueSetter={setPassword}
+          valueErrorSetter={setPasswordError}
+        />
+        <FormField
+          name={'Repeat Password'}
+          type={'password'}
+          value={repeatPassword}
+          valueError={repeatPasswordError}
+          valueSetter={setRepeatPassword}
+          valueErrorSetter={setRepeatPasswordError}
+        />
         <div className="flex items-center justify-between">
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
